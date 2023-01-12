d68b58f85ff3bf75eb98692fa4f17d75579334b8
Juho Poutiainen <j.poutiainen94@gmail.com>
2023-01-12 13:30:01 +0200

    Add route for single station

--- city-bikes-backend/src/controllers/stations.ts
+++ city-bikes-backend/src/controllers/stations.ts
@@ -1,8 +1,15 @@
-import express from "express"
+import * as express from "express"
+import { IStation } from "../models/station"
 
 const router = express.Router()
 
 const Station = require('../models/station')
+
+declare module 'express' {
+    export interface Request {
+        station?: IStation
+    }
+}
 
 // get stations on default 10 stations per page
 // default view/order -> ASC by station id
@@ -22,6 +29,21 @@ router.get('/', async (req, res) => {
 
     } catch (error:any) {
         res.status(400).json({ error: 'Error while getting stations..: ' + error.message})
+    }
+})
+
+// find corresponding station by id
+const stationFinder = async (req:express.Request, _res:express.Response, next: express.NextFunction) => {
+    req.station = await Station.findByPk(req.params.id)
+    next()
+}
+
+// get single station
+router.get('/:id', stationFinder, async (req:express.Request, res) => {
+    if (req.station) {
+        res.json(req.station)
+    } else {
+        res.status(404).send({ error: `Error.. Station not found with id: ${req.params.id}`})
     }
 })
 
