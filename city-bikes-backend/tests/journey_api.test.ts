8d52f73f95d517abfc43ff19cb4d1b7119b9f410
Juho Poutiainen <j.poutiainen94@gmail.com>
2023-01-18 19:01:52 +0200

    Add tests for journey api

--- city-bikes-backend/tests/journey_api.test.ts
+++ city-bikes-backend/tests/journey_api.test.ts
@@ -1,10 +1,27 @@
+const { connectToDatabase } = require('../src/utils/db')
 const supertest = require('supertest')
 
 const app = require('../src/app')
-const api = supertest(app)
+const api = supertest(app);
 
-test('check jest & supertest', async () => {
-    await api
-        .get('/')
-        .expect(200)
+// connect to the database before the tests
+(async () => {
+    await connectToDatabase()
 })
+
+describe('Jourey list tests', () => {
+    test('Journey lists returns with status code 200 on default query', async () => {
+        await api
+            .get('/api/journeys')
+            .expect('Content-Type', /json/)
+            .expect(200)
+    })
+    test('Returns with status code 200 on query containg valid limit and page', async () => {
+        await api
+            .get('/api/journeys?page=1&limit=2')
+            .expect('Content-Type', /json/)
+            .expect(200)
+    })
+})
+
+export {}
