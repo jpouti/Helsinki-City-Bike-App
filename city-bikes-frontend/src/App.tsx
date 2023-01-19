b90013144aa4deea55bea52110a7c64f2a49ce37
Juho Poutiainen <j.poutiainen94@gmail.com>
2023-01-19 13:35:57 +0200

    Add custom eslint & tsconfig rules

--- city-bikes-frontend/src/App.tsx
+++ city-bikes-frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Helsinki City Bikes
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                Helsinki City Bikes
+            </header>
+        </div>
+    );
 }
 
 export default App;
