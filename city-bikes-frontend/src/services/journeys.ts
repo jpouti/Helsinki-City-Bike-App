4f22251c5755acd6c77ded8f1daa38a730fccd44
Juho Poutiainen <j.poutiainen94@gmail.com>
2023-01-23 11:42:41 +0200

    Update journeys service

--- city-bikes-frontend/src/services/journeys.ts
+++ city-bikes-frontend/src/services/journeys.ts
@@ -6,15 +6,28 @@ const baseUrl = 'http://localhost:3001/api/journeys'
 
 // get journeys according to page and limit
 // returns journeys with default page and limit if paremeters not provided
-export default async function getJourneys(options: JourneyViewOptions) {
-    if (!options.page && !options.limit) {
-        const request = await axios.get<IJourney[]>(baseUrl)
-        return request.data
-    } else if (!options.page && options.limit) {
-        const request = await axios.get<IJourney[]>(`${baseUrl}?limit=${options.limit}`)
-        return request.data
-    } else if (options.page && options.limit) {
-        const request = await axios.get<IJourney[]>(`${baseUrl}?page=${options.page}&limit=${options.limit}`)
-        return request.data
+export const getJourneys = async (options: JourneyViewOptions):Promise<IJourney[] | string> => {
+    try {
+        // journeys with default options
+        if (!options.page && !options.limit) {
+            const request = await axios.get<IJourney[]>(baseUrl)
+            return request.data
+        // journeys with default page and provided limit
+        } else if (!options.page && options.limit) {
+            const request = await axios.get<IJourney[]>(`${baseUrl}?limit=${options.limit}`)
+            return request.data
+        // journeys with provided page and limit
+        } else if (options.page && options.limit) {
+            const request = await axios.get<IJourney[]>(`${baseUrl}?page=${options.page}&limit=${options.limit}`)
+            return request.data
+        }
+    } catch (error:unknown) {
+        let errorMessage = 'Error while fetching journeys data.'
+        if (axios.isAxiosError(error) && error.response) {
+            errorMessage = ' Error: ' + error.response.data.message
+        }
+        return errorMessage
     }
+
+    return 'Error while fetching journeys data.'
 }
