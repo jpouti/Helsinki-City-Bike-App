d99de9541c6505f35953b9d815213b78a45e93c5
Juho Poutiainen <j.poutiainen94@gmail.com>
2023-02-02 14:59:06 +0200

    Extend journeys service with order per column & refactor url parsing for service

--- city-bikes-frontend/src/services/journeys.ts
+++ city-bikes-frontend/src/services/journeys.ts
@@ -4,39 +4,65 @@ import { JourneysData, JourneyViewOptions } from '../types'
 // journeys api
 const baseUrl = 'http://localhost:3001/api/journeys'
 
+// parse url according to options parameters
+const parseUrl = (options: JourneyViewOptions) => {
+    let url = baseUrl
+
+    // with page
+    if (options.page) {
+        // if the first query on url
+        if (url.length === baseUrl.length) {
+            url += `?page=${options.page}`
+        } else {
+            url += `&page=${options.page}`
+        }
+    }
+
+    // with limit
+    if (options.limit) {
+        if (url.length === baseUrl.length) {
+            url += `?limit=${options.limit}`
+        } else {
+            url += `&limit=${options.limit}`
+        }
+    }
+
+    // with search
+    if (options.search) {
+        if (url.length === baseUrl.length) {
+            url += `?search=${options.search}`
+        } else {
+            url += `&search=${options.search}`
+        }
+    }
+
+    // with order -- api query for order is sort
+    if (options.order) {
+        if (url.length === baseUrl.length) {
+            url += `?sort=${options.order}`
+        } else {
+            url += `&sort=${options.order}`
+        }
+    }
+
+    // with orderBy -- api query for orderBy is column
+    if (options.orderBy) {
+        if (url.length === baseUrl.length) {
+            url += `?column=${options.orderBy}`
+        } else {
+            url += `&column=${options.orderBy}`
+        }
+    }
+    return url
+}
+
 // get journeys according to page & limit and optional search keyword (substring of departure / return stations)
 // returns journeys with default page and limit if paremeters not provided
 export const getJourneys = async (options: JourneyViewOptions):Promise<JourneysData | string> => {
+    const journeyUrl = parseUrl(options)
     try {
-        // journeys with default options
-        if (!options.page && !options.limit) {
-            if (!options.search) {
-                const request = await axios.get<JourneysData>(baseUrl)
-                return request.data
-            } else {
-                const request = await axios.get<JourneysData>(`${baseUrl}?search=${options.search}`)
-                return request.data
-            }
-        // journeys with default page and provided limit
-        } else if (!options.page && options.limit) {
-            if (!options.search) {
-                const request = await axios.get<JourneysData>(`${baseUrl}?limit=${options.limit}`)
-                return request.data
-            } else {
-                const request = await axios.get<JourneysData>(`${baseUrl}?limit=${options.limit}&search=${options.search}`)
-                return request.data
-            }
-
-        // journeys with provided page and limit
-        } else if (options.page && options.limit) {
-            if (!options.search) {
-                const request = await axios.get<JourneysData>(`${baseUrl}?page=${options.page}&limit=${options.limit}`)
-                return request.data
-            } else {
-                const request = await axios.get<JourneysData>(`${baseUrl}?page=${options.page}&limit=${options.limit}&search=${options.search}`)
-                return request.data
-            }
-        }
+        const request = await axios.get<JourneysData>(journeyUrl)
+        return request.data
     } catch (error:unknown) {
         console.log(error)
         let errorMessage = 'Error while fetching journeys data.'
@@ -45,6 +71,4 @@ export const getJourneys = async (options: JourneyViewOptions):Promise<JourneysD
         }
         return errorMessage
     }
-
-    return 'Error while fetching journeys data.'
 }
