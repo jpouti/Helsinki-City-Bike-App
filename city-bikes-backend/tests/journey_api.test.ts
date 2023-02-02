30c69688c483c03d2dbfa935671f2a81175b1a52
Juho Poutiainen <j.poutiainen94@gmail.com>
2023-02-02 11:26:45 +0200

    Extend journeys_api tests

--- city-bikes-backend/tests/journey_api.test.ts
+++ city-bikes-backend/tests/journey_api.test.ts
@@ -16,11 +16,20 @@ describe('Jourey list tests', () => {
             .expect('Content-Type', /json/)
             .expect(200)
     })
-    test('Returns with status code 200 on query containg valid limit & page and search', async () => {
+    test('Returns with status code 200 on query containg valid limit & page & search and order column & sort', async () => {
         await api
-            .get('/api/journeys?page=1&limit=2&search=tie')
+            .get('/api/journeys?page=1&limit=2&search=tie&column=distance&sort=ASC')
             .expect('Content-Type', /json/)
             .expect(200)
+    })
+    test('Invalid column to order gives status code 400, invalid query', async () => {
+        await api
+        .get('/api/journeys?column=dep&sort=ASC')
+        .expect(400)
+
+        await api
+        .get('/api/journeys?column=departure&sort=aaa')
+        .expect(400)
     })
 })
 
