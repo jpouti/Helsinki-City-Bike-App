3f68eb573f59b4596b48c547cdc97695a8c49da9
Juho Poutiainen <j.poutiainen94@gmail.com>
2023-02-04 00:25:40 +0200

    Update stations list with message when no stations found

--- city-bikes-frontend/src/components/stations/StationsList.tsx
+++ city-bikes-frontend/src/components/stations/StationsList.tsx
@@ -11,6 +11,7 @@ import TableRow from '@mui/material/TableRow'
 import Table from '@mui/material/Table'
 import TableFooter from '@mui/material/TableFooter'
 import TablePagination from '@mui/material/TablePagination'
+import Typography from '@mui/material/Typography'
 
 type StationItemProps = {
     station: IStation
@@ -56,8 +57,13 @@ type StationListProps = {
 const StationsList: React.FC<StationListProps> = ({ stationsData, page, limit, handlePageChange, handleLimitChange }) => {
     console.log(stationsData, 'stations data')
 
-    if (!stationsData) {
-        return null
+    // display message when no journeys found
+    if (stationsData.stations.length < 1) {
+        return (
+            <Card sx={{ p: 2 }}>
+                <Typography variant='h4' color='red'>Could not find any stations with current search</Typography>
+            </Card>
+        )
     }
 
     return (
